d185b9c8d0546d1fee2b023ee6b60eafe59d4dc9
SourMangoMan <ymazahim@gmail.com>
2024-06-13 13:07:09 +0300

    part e

--- src-linear/submission/gda.py
+++ src-linear/submission/gda.py
@@ -26,6 +26,49 @@ class GDA:
             y: Training example labels. Shape (n_examples,).
         """
         # *** START CODE HERE ***
+        x = np.array(x)
+        y = np.array(y)
+
+        n = x.shape[0]
+        no_of1s = 0
+        no_of0s = 0
+        mu0_top = 0
+        mu1_top = 0
+        sigma = np.zeros((x.shape[1], x.shape[1]))
+
+        for i in range(n):
+            no_of1s += y[i]
+            no_of0s += 1 - y[i]
+            mu0_top += (1 - y[i]) * x[i, :]
+            mu1_top += (y[i]) * x[i, :]
+            
+        phi = no_of1s / n
+        mu_0 = np.array(mu0_top / no_of0s).reshape((-1, 1))
+        
+        mu_1 = np.array(mu1_top / no_of1s).reshape((-1, 1))
+        
+
+        for i in range(n):
+            x_i = (x[i, :]).reshape((-1, 1))
+
+            if y[i] < 0.5:
+                sigma += (x_i - mu_0) @ (x_i - mu_0).T 
+            else:
+                sigma += (x_i - mu_1) @ (x_i - mu_1).T 
+
+        sigma /= n
+
+        # You need to calculate self.theta!!!!!!! 
+        S = np.linalg.inv(sigma)
+        theta = (S @ (mu_1 - mu_0))
+        theta_0 = 0.5 * (mu_0.T @ S @ mu_0 - mu_1.T @ S @ mu_1) - np.log((1 - phi) / phi)
+
+        if theta_0.shape != (1, 1):
+            theta_0 = theta_0.reshape((1, 1))
+
+        self.theta = np.vstack((theta_0, theta))
+        # self.theta = 
+
         # *** END CODE HERE ***
 
     def predict(self, x):
@@ -38,4 +81,12 @@ class GDA:
             Outputs of shape (n_examples,).
         """
         # *** START CODE HERE ***
+        x = np.array(x)
+        
+        
+        linear = (self.theta.T @ x.T).T
+        prediction = 1 / (1 + np.exp(-linear))
+        prediction = prediction.reshape(x.shape[0])
+
+        return (prediction >= 0.5).astype(int)
         # *** END CODE HERE
